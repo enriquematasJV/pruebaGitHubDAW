b36abdf89c090b4f91c2f0ee2109f6012587169c
Dani Rojas <danielvicente.rojas.catalan.alu@iesjulioverne.es>
2024-05-27 08:44:14 +0200

    Amoh a ver si va ;)

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -12,5 +12,6 @@ public class Mavenproject1 {
 
     public static void main(String[] args) {
         System.out.println("prueba ");
+        System.out.println("Esto es una prueba de gitHub");
     }
 }
